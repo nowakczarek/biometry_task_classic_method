5a69d9690b4326762c23dd505211d1db218d5932
nowakczarek <czarekkawon@onet.pl>
2024-11-25 21:15:52 +0100

    StartedWorkingOnPreprocessingAndSIFT

--- app.py
+++ app.py
@@ -2,28 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
-# image = cv2.imread('michal.jpg')
-# image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGBA)
-
-# plt.imshow(image)
-# plt.show()
-
-# plt.imshow(image_rgb[:, :, 2], cmap = 'gray')
-# plt.show()
-
-
-# v2
-
-# image = cv2.imread('michal.jpg', cv2.IMREAD_GRAYSCALE)
-
-# maximum_value = np.max(image)
-# threshold_type = cv2.THRESH_BINARY_INV
-# threshold_value = 125
-
-# ret, binary_image = cv2.threshold(image, threshold_value, maximum_value, threshold_type)
-# plt.imshow(binary_image, cmap='gray')
-# plt.show()
-
 img1 = cv2.imread('real/1__M_Left_index_finger.BMP', cv2.IMREAD_GRAYSCALE)
 img2 = cv2.imread('altered/altered_medium/1__M_Left_index_finger_CR.BMP', cv2.IMREAD_GRAYSCALE)
 
